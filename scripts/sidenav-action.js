7dccbe31aea5f10d7fdfce98718f9c694c2b7fb8
giga lomidze <gigalomidze94@gmail.com>
2019-11-29 00:24:56 +0400

    initial-version

---
+++ scripts/sidenav-action.js
@@ -0,0 +1,7 @@
+document.getElementById("close-icon").addEventListener("click", function () {
+    document.getElementById('wholesidenav').style.visibility = "hidden";
+});
+
+document.getElementById("menu-icon").addEventListener("click", function () {
+    document.getElementById('wholesidenav').style.visibility = "visible";
+});
